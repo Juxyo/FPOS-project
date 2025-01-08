6e833c9faefca6c03abb6d75cb98c1140f6c7688
Juxyo <94303202+Juxyo@users.noreply.github.com>
2025-01-08 11:57:09 +0100

    project-init

---
+++ src/main/java/com/todo/FPOS_project/FposProjectApplication.java
@@ -0,0 +1,13 @@
+package com.todo.FPOS_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FposProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FposProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo/FPOS_project/FposProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.FPOS_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FposProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
