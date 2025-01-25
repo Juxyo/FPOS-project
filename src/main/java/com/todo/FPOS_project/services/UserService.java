22d8f2964cd8a720f814495cffc9c88d1339d952
Demonis0 <demonisgames@gmail.com>
2025-01-25 22:45:08 +0100

    UserService

---
+++ src/main/java/com/todo/FPOS_project/services/UserService.java
@@ -0,0 +1,30 @@
+﻿package com.todo.FPOS_project.services;
+
+import com.todo.FPOS_project.db.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+    private final UserRepository userRepository;
+    
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    public boolean userIdExists(String nationalId) {
+        return userRepository.findByNationalId(nationalId) != null;
+    }
+    
+    public boolean userEmailExists(String emailAdress) {
+        return userRepository.findByEmailAdress(emailAdress) != null;
+    }
+    
+    public boolean isUserIdEnabled(String nationalId) {
+        return userRepository.findByNationalId(nationalId).isEnabled();
+    }
+    
+    public boolean isUserEmailEnabled(String emailAdress) {
+        return userRepository.findByEmailAdress(emailAdress).isEnabled();
+    }
+}
