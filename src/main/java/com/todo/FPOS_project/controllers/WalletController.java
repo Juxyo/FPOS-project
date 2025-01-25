6544bd3341d8f071cce29f4a5b3d514a6cb2f0e7
Demonis0 <demonisgames@gmail.com>
2025-01-25 22:46:23 +0100

    WalletController

---
+++ src/main/java/com/todo/FPOS_project/controllers/WalletController.java
@@ -0,0 +1,39 @@
+﻿package com.todo.FPOS_project.controllers;
+
+import com.todo.FPOS_project.db.models.User;
+import com.todo.FPOS_project.services.UserService;
+import com.todo.FPOS_project.services.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RequestMapping("/wallet")
+@CrossOrigin(origins = "*")
+@RestController
+public class WalletController {
+    
+    @Autowired
+    private final WalletService walletService;
+    @Autowired
+    private final UserService userService;
+    
+    public WalletController(WalletService walletService, UserService userService) {
+        this.walletService = walletService;
+        this.userService = userService;
+    }
+    
+    @GetMapping("/balance")
+    public ResponseEntity getBalance(@RequestParam String investorId) {
+        if (investorId == null) return ResponseEntity.badRequest().body(Map.of("message", "Id is required."));
+
+        if (!userService.userIdExists(investorId)) return ResponseEntity.badRequest().body(Map.of("message", "User does not exist."));
+        if (!userService.isUserIdEnabled(investorId)) return ResponseEntity.badRequest().body(Map.of("message", "User is disabled."));
+        
+        if (!walletService.walletExists(investorId)) walletService.createWallet(investorId);
+        
+        return ResponseEntity.ok(Map.of("balance", walletService.getBalanceByInvestorId(investorId)));
+    }
+    
+}
