993a98aded4d81d357d17e59fcc1ace8752ba554
Demonis0 <demonisgames@gmail.com>
2025-01-25 02:34:03 +0100

    UserRepository

---
+++ src/main/java/com/todo/FPOS_project/db/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+﻿package com.todo.FPOS_project.db.repositories;
+
+import com.todo.FPOS_project.db.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Component
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    
+    @Query("{ 'emailAdress' : ?0 }")
+    User findByEmailAdress(String emailAdress);
+    
+    @Query("{ 'nationalId' : ?0 }")
+    User findByNationalId(String nationalId);
+    
+}
