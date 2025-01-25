29ef50ba4d4e4f85fb07325246e5f5f185267602
Demonis0 <demonisgames@gmail.com>
2025-01-25 05:24:17 +0100

    UserLoginDTO

---
+++ src/main/java/com/todo/FPOS_project/dtos/request/UserLoginDTO.java
@@ -0,0 +1,31 @@
+﻿package com.todo.FPOS_project.dtos.request;
+
+public class UserLoginDTO {
+    
+    private String emailAdress, password;
+
+    public UserLoginDTO() {
+    }
+
+    public UserLoginDTO(String emailAdress, String password) {
+        this.emailAdress = emailAdress;
+        this.password = password;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
