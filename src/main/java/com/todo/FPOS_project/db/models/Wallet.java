65a5dbda0f27dacf142b4b4f63ae0c2b94e31b75
Demonis0 <demonisgames@gmail.com>
2025-01-25 03:15:10 +0100

    Wallet

---
+++ src/main/java/com/todo/FPOS_project/db/models/Wallet.java
@@ -0,0 +1,34 @@
+﻿package com.todo.FPOS_project.db.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Wallets")
+public class Wallet {
+    
+    private String investorId;
+    private double balance;
+    
+    public Wallet() {
+    }
+    
+    public Wallet(String investorId, double balance) {
+        this.investorId = investorId;
+        this.balance = balance;
+    }
+    
+    public String getInvestorId() {
+        return investorId;
+    }
+    
+    public void setInvestorId(String investorId) {
+        this.investorId = investorId;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
