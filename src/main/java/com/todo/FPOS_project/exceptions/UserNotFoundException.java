ef0f746de058b7c90683d5f48384d561734da266
Demonis0 <demonisgames@gmail.com>
2025-01-25 03:03:33 +0100

    UserNotFoundException

---
+++ src/main/java/com/todo/FPOS_project/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+﻿package com.todo.FPOS_project.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
