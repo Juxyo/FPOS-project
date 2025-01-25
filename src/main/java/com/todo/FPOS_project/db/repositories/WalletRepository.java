023f4d55593a407c1b8661f279ab267d0619171b
Demonis0 <demonisgames@gmail.com>
2025-01-25 23:08:58 +0100

    fixes

--- src/main/java/com/todo/FPOS_project/db/repositories/WalletRepository.java
+++ src/main/java/com/todo/FPOS_project/db/repositories/WalletRepository.java
@@ -1,6 +1,5 @@
 package com.todo.FPOS_project.db.repositories;
 
-import com.todo.FPOS_project.db.models.User;
 import com.todo.FPOS_project.db.models.Wallet;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
