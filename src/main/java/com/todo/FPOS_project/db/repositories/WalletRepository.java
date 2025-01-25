eb433fe424069b296061c6399f5956cefb431fb6
Demonis0 <demonisgames@gmail.com>
2025-01-25 22:45:35 +0100

    WalletRepository

---
+++ src/main/java/com/todo/FPOS_project/db/repositories/WalletRepository.java
@@ -0,0 +1,15 @@
+﻿package com.todo.FPOS_project.db.repositories;
+
+import com.todo.FPOS_project.db.models.User;
+import com.todo.FPOS_project.db.models.Wallet;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface WalletRepository extends MongoRepository<Wallet, String> {
+    
+    @Query("{ 'investorId' : ?0 }")
+    Wallet findByInvestorId(String investorId);
+    
+    @Query(value = "{ 'investorId' : ?0 }", fields = "{ 'balance' : 1 }")
+    double getBalanceByInvestorId(String investorId);
+}
