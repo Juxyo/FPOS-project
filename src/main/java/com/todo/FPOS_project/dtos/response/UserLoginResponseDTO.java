aa335b6f319b7c02a2f509d4914ae3e681c562cf
Demonis0 <demonisgames@gmail.com>
2025-01-25 05:24:58 +0100

    UserLoginResponseDTO

---
+++ src/main/java/com/todo/FPOS_project/dtos/response/UserLoginResponseDTO.java
@@ -0,0 +1,36 @@
+﻿package com.todo.FPOS_project.dtos.response;
+
+import java.time.LocalDateTime;
+import java.util.PrimitiveIterator;
+
+public class UserLoginResponseDTO {
+
+    private String value;
+    private LocalDateTime expiresAt;
+
+    public UserLoginResponseDTO() {
+    }
+
+    public UserLoginResponseDTO(String value, LocalDateTime expiresAt) {
+        this.value = value;
+        this.expiresAt = expiresAt;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public UserLoginResponseDTO setValue(String value) {
+        this.value = value;
+        return this;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public UserLoginResponseDTO setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+        return this;
+    }
+}
