26eb2c175f6c0edf81cf3905a69f5eefc7203387
Demonis0 <demonisgames@gmail.com>
2025-01-25 04:02:42 +0100

    Share

---
+++ src/main/java/com/todo/FPOS_project/db/models/Share.java
@@ -0,0 +1,45 @@
+﻿package com.todo.FPOS_project.db.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Shares")
+public class Share {
+    
+    @Id
+    private String investorId, propertyId;
+    private double sharePercent;
+
+    public Share() {
+    }
+
+    public Share(String investorId, String propertyId, double sharePercent) {
+        this.investorId = investorId;
+        this.propertyId = propertyId;
+        this.sharePercent = sharePercent;
+    }
+
+    public String getInvestorId() {
+        return investorId;
+    }
+
+    public void setInvestorId(String investorId) {
+        this.investorId = investorId;
+    }
+
+    public String getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(String propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public double getSharePercent() {
+        return sharePercent;
+    }
+
+    public void setSharePercent(double sharePercent) {
+        this.sharePercent = sharePercent;
+    }
+}
